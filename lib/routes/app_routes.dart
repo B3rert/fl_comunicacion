f670892cbe369219df21903ce42e108085d66738
Robert L <robertmorales181@gmail.com>
2023-05-31 11:28:26 -0600

    initial views

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,19 @@
+import 'package:fl_comunicacion/views/view.dart';
+import 'package:flutter/material.dart';
+
+class AppRoutes {
+  static const initialRoute = 'login';
+  static const secondRoute = 'home';
+  static Map<String, Widget Function(BuildContext)> routes = {
+    secondRoute: (BuildContext context) => const HomeView(),
+    initialRoute: (BuildContext context) => const LoginView(),
+    'api': (BuildContext context) => const ApiView(),
+  };
+
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    //print(settings);
+    return MaterialPageRoute(
+      builder: (BuildContext context) => const HomeView(),
+    );
+  }
+}
---
+++ lib/views/view.dart
@@ -0,0 +1,3 @@
+export 'package:fl_comunicacion/views/api_view.dart';
+export 'package:fl_comunicacion/views/home_view.dart';
+export 'package:fl_comunicacion/views/login_view.dart';
