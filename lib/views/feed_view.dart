e1e34a95b60bf2957eb063397b5e71c0182786ee
Robert L <robertmorales181@gmail.com>
2023-06-06 08:28:58 -0600

    ad screemn

--- lib/views/feed_view.dart
+++ lib/views/feed_view.dart
@@ -5,7 +5,8 @@ class FeedView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(),
       body: Center(
         child: Text('FeedView'),
       ),
