d5c4c4c07ed1173ce104d7253097438d5385bad5
Robert L <robertmorales181@gmail.com>
2023-06-06 11:03:18 -0600

    user info desogn

--- lib/views/feed_view.dart
+++ lib/views/feed_view.dart
@@ -30,9 +30,86 @@ class FeedView extends StatelessWidget {
           ),
         ),
       ),
-      drawer: Drawer(),
-      body: Center(
+      drawer: const _MyDrawer(
+        userEmail: "juanx64wndaowknol",
+        userName: "juan",
+        userPhotoUrl: "https://cdn-icons-png.flaticon.com/512/1946/1946429.png",
+      ),
+      body: const Center(
         child: Text('FeedView'),
+      ),
+    );
+  }
+}
+
+class _MyDrawer extends StatelessWidget {
+  final String userName;
+  final String userEmail;
+  final String userPhotoUrl;
+
+  const _MyDrawer({
+    required this.userName,
+    required this.userEmail,
+    required this.userPhotoUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: AppTheme.backroundColor,
+      child: Column(
+        children: [
+          UserAccountsDrawerHeader(
+            decoration: const BoxDecoration(
+              color: AppTheme.primary,
+            ),
+            accountName: Text(userName),
+            accountEmail: Text(userEmail),
+            currentAccountPicture: const CircleAvatar(
+              backgroundColor: AppTheme.backroundColorSecondary,
+              radius: 32,
+              child: FadeInImage(
+                placeholder: AssetImage(
+                  "assets/user.png",
+                ),
+                image: NetworkImage(
+                  "https://cdn-icons-png.flaticon.com/512/1946/1946429.png",
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                // ... más elementos del Drawer
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+                height: 56,
+                color: AppTheme.backroundColorSecondary,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.settings,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.logout,
+                      ),
+                    )
+                  ],
+                )),
+          ),
+        ],
       ),
     );
   }
