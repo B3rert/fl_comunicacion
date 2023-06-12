598dbbc91e436d67ba2c423992d895a0e7956f6e
Robert L <robertmorales181@gmail.com>
2023-06-12 12:06:34 -0600

    desing view

--- lib/view_models/new_post_view_model.dart
+++ lib/view_models/new_post_view_model.dart
@@ -1,3 +1,24 @@
 import 'package:flutter/material.dart';
 
-class NewPostViewModel extends ChangeNotifier {}
+class NewPostViewModel extends ChangeNotifier {
+  bool isLoading = false;
+
+  //Map form
+  final Map<String, String> formValues = {
+    'title': '',
+    'description': '',
+  };
+
+  //Key for form
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  //True if form is valid
+  bool isValidForm() {
+    return formKey.currentState?.validate() ?? false;
+  }
+
+  createPost() {
+    if (!isValidForm()) return;
+    print(formValues);
+  }
+}
--- lib/views/new_post_view.dart
+++ lib/views/new_post_view.dart
@@ -1,23 +1,45 @@
+import 'package:fl_comunicacion/view_models/new_post_view_model.dart';
 import 'package:fl_comunicacion/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class NewPostView extends StatelessWidget {
   const NewPostView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final _vm = Provider.of<NewPostViewModel>(context);
+
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _vm.createPost(),
+        child: const Icon(Icons.save),
+      ),
       appBar: AppBar(),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: [
-              InputWidget(
-                  maxLines: maxLines,
-                  formProperty: formProperty,
-                  formValues: formValues)
-            ],
+          padding: const EdgeInsets.all(20),
+          child: Form(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            key: _vm.formKey,
+            child: Column(
+              children: [
+                InputWidget(
+                  hintText: "Titulo",
+                  labelText: "Titutlo",
+                  maxLines: 1,
+                  formProperty: "title",
+                  formValues: _vm.formValues,
+                ),
+                InputWidget(
+                  hintText: "Descripcion",
+                  labelText: "Descripcion",
+                  maxLines: 4,
+                  formProperty: "description",
+                  formValues: _vm.formValues,
+                ),
+              ],
+            ),
           ),
         ),
       ),
